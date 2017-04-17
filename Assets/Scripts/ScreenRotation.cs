e911c2a9f6987cc69e7b87754d082bd01eef0c9a
Stef <stef.baeyens@gmail.com>
2017-04-17 15:00:02 +0200

    Eerste Test

---
+++ Assets/Scripts/ScreenRotation.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenRotation : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.touchCount > 0) {
+			float yRotation = 90.0f;
+			transform.eulerAngles = new Vector3(transform.eulerAngles.x, yRotation, transform.eulerAngles.z);
+		}
+	}
+}
