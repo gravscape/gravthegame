dc9e23150331fafb28d3ac746bc2c4ccc7669189
Stef <stef.baeyens@gmail.com>
2017-04-17 16:23:39 +0200

    Gravity

--- Assets/Scripts/ScreenRotation.cs
+++ Assets/Scripts/ScreenRotation.cs
@@ -4,16 +4,26 @@ using UnityEngine;
 
 public class ScreenRotation : MonoBehaviour {
 
+	public float smooth = 1f;
+	private Quaternion targetRotation;
+	private int angle = 0;
+	public Rigidbody rig;
+	public float grav = 9.81f;
+
 	// Use this for initialization
 	void Start () {
-		
+		targetRotation = transform.rotation;
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-		if (Input.touchCount > 0) {
-			float yRotation = 90.0f;
-			transform.eulerAngles = new Vector3(transform.eulerAngles.x, yRotation, transform.eulerAngles.z);
+		if (Input.GetKeyDown (KeyCode.Space)) { 
+			angle++;
+			targetRotation *=  Quaternion.AngleAxis(90, Vector3.back);
+			rig.velocity = Vector3.zero;
+			float radAngle = (90 * angle) * (Mathf.PI / 180);
+			Physics.gravity = new Vector3 (-Mathf.Round(Mathf.Sin (radAngle)) * grav,Mathf.Round(-Mathf.Cos (radAngle)) * grav,0);
 		}
+		transform.rotation= Quaternion.Lerp (transform.rotation, targetRotation , 10 * smooth * Time.deltaTime); 
 	}
 }
