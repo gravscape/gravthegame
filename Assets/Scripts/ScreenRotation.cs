84a939751e51ab01744a458285fd793e54f0edff
Stef <stef.baeyens@gmail.com>
2017-04-18 18:54:55 +0200

    Left and Right

--- Assets/Scripts/ScreenRotation.cs
+++ Assets/Scripts/ScreenRotation.cs
@@ -15,15 +15,29 @@ public class ScreenRotation : MonoBehaviour {
 		targetRotation = transform.rotation;
 	}
 
-	// Update is called once per frame
-	void Update () {
-		if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Ended) { 
+	enum Richting{
+		L, R
+	}
+
+	void Rotate(Richting richting){
+		switch (richting) {
+		case Richting.L:
 			angle++;
 			targetRotation *=  Quaternion.AngleAxis(90, Vector3.back);
 			rig.velocity = Vector3.zero;
 			float radAngle = (90 * angle) * (Mathf.PI / 180);
 			Physics.gravity = new Vector3 (-Mathf.Round(Mathf.Sin (radAngle)) * grav,Mathf.Round(-Mathf.Cos (radAngle)) * grav,0);
+			break;
+		case Richting.R:
+			break;
 		}
-		transform.rotation= Quaternion.Lerp (transform.rotation, targetRotation , 10 * smooth * Time.deltaTime); 
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Ended) { 
+			Rotate (Richting.L);
+		} 
+		transform.rotation= Quaternion.Lerp (transform.rotation, targetRotation , 10 * smooth * Time.deltaTime);
 	}
 }
