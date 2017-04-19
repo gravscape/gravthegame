aeb2db4bc5bfe30fabf4b45f409bd7995bd2d093
Stef <stef.baeyens@gmail.com>
2017-04-19 21:43:58 +0200

    Spawn new piece

--- Assets/Scripts/PitGeneration.cs
+++ Assets/Scripts/PitGeneration.cs
@@ -8,30 +8,30 @@ public class PitGeneration : MonoBehaviour {
 	public Transform parent;
 	public Transform player;
 
-	private GameObject[] generatedPieces;
+	private List<GameObject> generatedPieces = new List<GameObject>();
 	private int location = 1;
 
 	// Use this for initialization
 	void Start () {
-		generatedPieces = new GameObject[50];
-		generatedPieces [1] = Instantiate (prefabPieces [0], parent);
-		generatedPieces [1].transform.position = new Vector3 (0, 10, 0);
-		generatedPieces [1] = Instantiate (prefabPieces [1], parent);
+		generatedPieces.Add(Instantiate (prefabPieces [0], parent));
+		generatedPieces[0].transform.position = new Vector3 (0, 10, 0);
+		generatedPieces.Add(Instantiate (prefabPieces [1], parent));
 		generatedPieces [1].transform.position = Vector3.zero;
 		for(int i = 0; i < 3; i++){
 			generatedPieces [1].transform.GetChild (i).localScale += new Vector3(0,10,0);
 		}
-		Debug.Log(generatedPieces [1].transform.lossyScale.y);
 	}
 
 	// Update is called once per frame
 	void Update () {
 		if (player.position.y < -generatedPieces [location].transform.GetChild(0).localScale.y / 2) {
 			location++;
-
-            int index = generatedPieces.Length;
-            generatedPieces[index] = Instantiate(prefabPieces[1], parent);
-            generatedPieces[index].transform.GetChild(0).localScale += new Vector3(0, 10, 0);
+			GameObject temp = Instantiate (prefabPieces [1], parent);
+			generatedPieces.Add(temp);
+			for(int i = 0; i < temp.transform.childCount; i++){
+				temp.transform.position = new Vector3 (0, -7.5f, 0);
+				temp.transform.GetChild(i).localScale += new Vector3(0, 10, 0);
+			}
         }
 		Debug.Log (location);
 
